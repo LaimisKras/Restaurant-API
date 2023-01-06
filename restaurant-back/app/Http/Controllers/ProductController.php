bbf0d892912f31dcb5f5eff99908bd7d921d4ca8
Laimis Krasauskas <laimis.kras@gmail.com>
2023-01-06 14:05:11 +0200

    Create Restaurant API

---
+++ restaurant-back/app/Http/Controllers/ProductController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    //
+    function addProduct (Request $req) {
+        
+        $product = new Product;
+        $product -> name = $req -> input ('name');
+        $product -> price = $req -> input ('price');
+        $product -> description = $req -> input ('description');
+        $product -> file_path = $req -> file ('file') -> store ('products');
+        $product -> save ();
+
+        return $product;
+    }
+
+    function list () {
+        return Product::all();
+    }
+
+    function delete ($id) {
+        $result = Product::where ('id', $id) -> delete ();
+        if ($result) {
+            return ["result" => "Product has been delete"];
+        } else {
+            return ["result" => "Operation failed"]; 
+        }
+    }
+
+}
